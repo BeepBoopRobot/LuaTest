d5cfa46580da13fdff96a763cfc59f47ee24857b
beepbooprobot <ben@9xa.net>
2017-10-04 15:57:45 +0100

    Initial commit

---
+++ .idea/Table.lua
@@ -0,0 +1,12 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: E7270
+-- Date: 04/10/2017
+-- Time: 15:27
+-- To change this template use File | Settings | File Templates.
+--
+
+x= { }
+x['primary'] = 'Monkey'
+x['foreign'] = 'spy'
+print(x['primary'])
